d77f79e21d0df08f422fff95644e8071d380984a
rvck2007 <sekaherve@gmail.com>
2020-10-10 06:27:34 +0200

    Validation avec messages d'erreurs contextuels aux champs
    Les messages d'erreurs s'affichent au nievau (en-dessous) des champs concernés.

--- resources/views/components/create.blade.php
+++ resources/views/components/create.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<!--
 @if($errors->any())
     <div class="alert alert-danger">
         <ul>
@@ -10,19 +11,30 @@
         </ul>
     </div>
 @endif
+-->
 <form method="post" action="{{ route('article.store') }}">
     <!-- Attention, sans la directive csrf, le formulaire ne fonctionnera pas ! -->
     @csrf
     <div class="form-row">
       <div class="form-group col-md-6">
         <label for="title">Titre</label>
-        <input type="text" class="form-control" id="title" name="title" placeholder="Mon Titre">
+        <input type="text" class="form-control" id="title" name="title" @error('title') is-invalid @enderror placeholder="Mon Titre">
+        @error('title')
+            <div class="alert alert-danger mt-2">
+                {{ $message }}
+            </div>
+        @enderror
       </div>
     </div>
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="content">Contenu</label>
-            <textarea  class="form-control" id="content" name="content" placeholder="Entrez votre contenu"></textarea>
+            <textarea  class="form-control" id="content" name="content" @error('content') is-invalid @enderror placeholder="Entrez votre contenu"></textarea>
+            @error('content')
+            <div class="alert alert-danger mt-2">
+                {{ $message }}
+            </div>
+        @enderror
           </div>
     </div>
 
