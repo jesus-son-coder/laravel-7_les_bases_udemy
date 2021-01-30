f1fe77760aeb40fbfde139e01f474fbd23e5ac0d
hseka <sekaherve@gmail.com>
2021-01-30 23:11:36 +0100

    Génération de plusieurs données "Courses" avec le Seeder

--- database/seeders/CoursesSeeder.php
+++ database/seeders/CoursesSeeder.php
@@ -18,12 +18,28 @@ class CoursesSeeder extends Seeder
         $course = new Course();
         $course->title = "Les bases de Symfony 4";
         $course->subtitle = "Apprendre à créer un site avec Symfony 4";
-        $course->description = "That’s it! Now, if a flash message exists, we just need to print it on the page. Let’s do that in base.html.twig. The session object is available via app.session. Use it to check to see if we have any success flash messages. If we do, let’s print the messages inside a styled container. You’ll typically only have one message at a time, but the flash bag is flexible enough to store any number";
-        $course->price = 19.99;
-
         $slugify = new Slugify();
         $course->slug = $slugify->slugify($course->title);
+        $course->description = "That’s it! Now, if a flash message exists, we just need to print it on the page. Let’s do that in base.html.twig. The session object is available via app.session. Use it to check to see if we have any success flash messages. If we do, let’s print the messages inside a styled container. You’ll typically only have one message at a time, but the flash bag is flexible enough to store any number";
+        $course->price = 19.99;
+        $course->save();
 
+        $course = new Course();
+        $course->title = "Wordpress et création de site ecommerce";
+        $course->subtitle = "Construire un site emcommerce complet avec Wordpress";
+        $slugify = new Slugify();
+        $course->slug = $slugify->slugify($course->title);
+        $course->description = "That’s it! Now, if a flash message exists, we just need to print it on the page. Let’s do that in base.html.twig. The session object is available via app.session. Use it to check to see if we have any success flash messages. If we do, let’s print the messages inside a styled container. You’ll typically only have one message at a time, but the flash bag is flexible enough to store any number";
+        $course->price = 14.99;
+        $course->save();
+
+        $course = new Course();
+        $course->title = "Les bases de Laravel 7";
+        $course->subtitle = "Créer une plateforme d'enseignement avec Laravel 7";
+        $slugify = new Slugify();
+        $course->slug = $slugify->slugify($course->title);
+        $course->description = "That’s it! Now, if a flash message exists, we just need to print it on the page. Let’s do that in base.html.twig. The session object is available via app.session. Use it to check to see if we have any success flash messages. If we do, let’s print the messages inside a styled container. You’ll typically only have one message at a time, but the flash bag is flexible enough to store any number";
+        $course->price = 39.99;
         $course->save();
     }
 }
