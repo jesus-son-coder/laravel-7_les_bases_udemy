ebf533fddbdc67240ebcad8ac14990ce45c89ec4
hseka <sekaherve@gmail.com>
2021-01-31 06:52:20 +0100

    Exiger l'Authentification pour afficher la section "Instructor"
    Insérer le Middleware "Auth" dans le Constructeur de "InstructorController" afin que toutes les méthodes de ce contrôleur nécessitent une Authentification avant de pouvoir s'afficher dans le navigateur

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -7,6 +7,11 @@ use \Illuminate\Http\Response;
 
 class InstructorController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
