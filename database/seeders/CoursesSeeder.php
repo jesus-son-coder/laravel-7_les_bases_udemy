17fe12415a06643ec86d42915044f3a59f2df690
hseka <sekaherve@gmail.com>
2021-01-31 06:24:52 +0100

    Générer les données pour la relation entre User et Courses

--- database/seeders/CoursesSeeder.php
+++ database/seeders/CoursesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Course;
+use App\Models\User;
 use Cocur\Slugify\Slugify;
 use Illuminate\Database\Seeder;
 
@@ -25,6 +26,7 @@ class CoursesSeeder extends Seeder
         $course->price = 19.99;
         // Récupérer un Id aléatoire dans la table Category :
         $course->category_id = Category::all()->random(1)->first()->id;
+        $course->user_id = User::all()->random(1)->first()->id;
         $course->save();
 
         $course = new Course();
@@ -35,6 +37,7 @@ class CoursesSeeder extends Seeder
         $course->description = "That’s it! Now, if a flash message exists, we just need to print it on the page. Let’s do that in base.html.twig. The session object is available via app.session. Use it to check to see if we have any success flash messages. If we do, let’s print the messages inside a styled container. You’ll typically only have one message at a time, but the flash bag is flexible enough to store any number";
         $course->price = 14.99;
         $course->category_id = Category::all()->random(1)->first()->id;
+        $course->user_id = User::all()->random(1)->first()->id;
         $course->save();
 
         $course = new Course();
@@ -45,6 +48,7 @@ class CoursesSeeder extends Seeder
         $course->description = "That’s it! Now, if a flash message exists, we just need to print it on the page. Let’s do that in base.html.twig. The session object is available via app.session. Use it to check to see if we have any success flash messages. If we do, let’s print the messages inside a styled container. You’ll typically only have one message at a time, but the flash bag is flexible enough to store any number";
         $course->price = 39.99;
         $course->category_id = Category::all()->random(1)->first()->id;
+        $course->user_id = User::all()->random(1)->first()->id;
         $course->save();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(CategoriesSeeder::class);
+        $this->call(UsersSeeder::class);
         $this->call(CoursesSeeder::class);
-        // $this->call(CategoriesSeeder::class);
     }
 }
 
