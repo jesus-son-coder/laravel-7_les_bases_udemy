a07253f66db5f4b8e79a830e2e23b18174f8f1ba
hseka <sekaherve@gmail.com>
2021-01-31 23:56:54 +0100

    Rendre dynamique l'affichage des vidéos et des infos liées

--- resources/views/instructor/curriculum/index.blade.php
+++ resources/views/instructor/curriculum/index.blade.php
@@ -8,7 +8,7 @@
             <div class="col-lg-12">
                 <div class="section-title">
                     <h2>Programme</h2>
-                    <p>Titre du cours</p>
+                    <p>{{ $course->title }}</p>
                 </div>
             </div>
         </div>
@@ -26,35 +26,39 @@
                                         Ajouter une section
                                     </a>
                                 </div>
-                                <div class="st-content">
-                                    <div class="container">
-                                        <div class="row">
-                                            <div class="col-lg-4 px3 py-3">
-                                                <iframe width="320" height="180" style="max-width: 100%"
-                                                    src="https://www.youtube.com/embed/tgbNymZ7vqY">
-                                                </iframe>
-                                            </div>
-                                            <div class="col-lg-4 text-left">
-                                                <div class="sc-text">
-                                                    <h4>Nom de la section</h4>
+
+                                @foreach($course->sections as $section)
+                                    <div class="st-content">
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col-lg-4 px3 py-3">
+                                                    <video width="300" height="180" style="max-width: 100%" controls>
+                                                        <source src="{{ asset('storage/courses_sections/' . $course->user_id . '/' . $section->video) }}" type="video/mp4">
+                                                    </video>
                                                 </div>
-                                                <p>Durée de la section : 00.50</p>
-                                            </div>
-                                            <div class="col-lg-4">
-                                                <div class="sc-text d-flex justify-content-around">
-                                                    <a class="btn btn-danger" href="#">
-                                                        <i class="fas fa-trash"></i>
-                                                        Supprimer
-                                                    </a>
-                                                    <a class="btn btn-warning" href="#">
-                                                        <i class="fas fa-edit"></i>
-                                                        Modifier
-                                                    </a>
+                                                <div class="col-lg-4 text-left">
+                                                    <div class="sc-text">
+                                                        <h4>{{ $section->name }}</h4>
+                                                    </div>
+                                                    <p>Durée de la section : {{ $section->playtime_seconds }}</p>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="sc-text d-flex justify-content-around">
+                                                        <a class="btn btn-danger" href="#">
+                                                            <i class="fas fa-trash"></i>
+                                                            Supprimer
+                                                        </a>
+                                                        <a class="btn btn-warning" href="#">
+                                                            <i class="fas fa-edit"></i>
+                                                            Modifier
+                                                        </a>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                @endforeach
+
 
                             @else
                                 <div class="text-center">
